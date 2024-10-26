8e95470b7c26ff4ca5ccffbd933f581839ad0c9c
Mohammad Billah <sefaislam530@gmail.com>
2024-10-26 15:42:16 +0600

    Update uptime.js

--- scripts/commands/uptime.js
+++ scripts/commands/uptime.js
@@ -120,8 +120,8 @@ registerFont(__dirname + `/nayan/CaviarDreams.ttf`, {
     });
     ctx.textAlign = "start";
     ctx.font = "45px time";
-    ctx.fillText("@" + "www.xnxx.com169", 250, 515)
-    ctx.fillText("@" + "MOHAMMAD-NAYAN", 250, 575)
+    ctx.fillText("@" + "www.mohammad.rahay", 250, 515)
+    ctx.fillText("@" + "MOHAMMAD-RAHAT", 250, 575)
    //ctx.fillText("@" + "DVFB.VietLe.pro", 405, 750)
     ctx.restore();
     ctx.save();
